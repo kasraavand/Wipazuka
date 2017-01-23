0f723113d5c373779cca383c362f642d67c4e1dd
kasramvd <kasraavand@gmail.com>
2017-01-23 15:14:45 +0330

    update persian wordner creator and add a general persian wordnet

--- Persian/persian.py
+++ Persian/persian.py
@@ -1,10 +1,52 @@
 """."""
-import WipazukaWasteWi
+import json
+import csv
 
 
-class PersianWipazuka(WipazukaWasteWi):
-    """."""
+class PersianWipazuka:
+    """Creat a WordNet from the corresponding json file."""
 
     def __init__(self, *args, **kwargs):
-        """."""
-    
+        """Initializer."""
+        inp_file_name = kwargs['inp_file_name']
+        self.wordnet_name = kwargs['wordnet_name']
+        self.raw_data = self.read_input(inp_file_name)
+        self.wordnet = self.load_wordnet()
+
+    def read_input(self, file_name):
+        with open(file_name) as f:
+            return json.load(f)
+
+    def load_wordnet(self):
+        d = {}
+        with open('general_wordnet/wn-data-fas.tab', encoding='utf8') as f:
+            reader = csv.reader(f, delimiter='\t')
+            next(reader)
+            for id_type, lemma, word in reader:
+                id_, type_ = id_type.split('-')
+                d[word] = {'id': id_, 'type': type_}
+            return d
+
+    def create_new_keywords(self):
+        for d in self.raw_data:
+            key_words = d['key_words']
+            new_key_words = []
+            for word in key_words:
+                try:
+                    type_ = d.get(word)['type']
+                except TypeError:
+                    new_key_words.append(word)
+                else:
+                    if type_ == 'n':
+                        new_key_words.append(word)
+            d['key_words'] = new_key_words
+            yield d
+
+    def create_wordnet(self):
+        with open(self.wordnet_name, 'w') as f:
+            json.dump(list(self.create_new_keywords()), f, indent=4)
+
+
+PW = PersianWipazuka(inp_file_name='persian.json',
+                     wordnet_name='date_wordnet.json')
+PW.create_wordnet()
